08466a89cee0be898b5eac3afb3882e0476ef7a4
Leandro da Silva <leandroda.silva@seb.com.br>
2025-10-04 19:44:32 -0300

    feat: Add play-button component and update package.json to version 2.7.0

--- src/mission-step.ts
+++ src/mission-step.ts
@@ -4,30 +4,31 @@ import {customElement} from 'lit/decorators.js';
 @customElement('mission-step')
 export class MissionStep extends LitElement {
   static override styles = css`
-      :host {
-          width: 100%;
-          height: 100%;
-      }
-      
-      .card {
-          display: block;
-          box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-          border-radius: 1.5rem;
-          backdrop-filter: blur(10px);
-          color: white;
-          border-color: rgba(0, 183, 215, 0.2);
-          border-style: solid;
-          border-width: 1px;
-          width: 100%;
-          height: 100%;
-          background-color: color-mix(in oklab, #0f172b 70%, transparent);
-      }
+    :host {
+      width: 100%;
+      height: 100%;
+    }
+
+    .card {
+      display: block;
+      box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+      border-radius: 1.5rem;
+      backdrop-filter: blur(10px);
+      color: white;
+      border-color: rgba(0, 183, 215, 0.2);
+      border-style: solid;
+      border-width: 1px;
+      width: 100%;
+      height: 100%;
+      background-color: color-mix(in oklab, #0f172b 70%, transparent);
+    }
   `;
 
   override render() {
     return html`
       <div class="card">
-        mission-step works!
+        <p>Mission</p>
+        <play-button></play-button>
       </div>
     `;
   }
---
+++ src/play-button.ts
@@ -0,0 +1,85 @@
+import {css, html, LitElement} from 'lit';
+import {customElement, query} from 'lit/decorators.js';
+
+@customElement('play-button')
+export class PlayButton extends LitElement {
+  static override styles = css`
+      :host {
+          height: 100%;
+          max-width: 160px;
+          max-height: 60px;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+      }
+      
+      .button-container {
+          width: 100%;
+          height: 100%;
+          max-width: 160px;
+          max-height: 60px;
+          position: relative;
+          overflow: hidden;
+          border-radius: 15px;
+      }
+      
+      canvas {
+          position: absolute;
+          top: -70px;
+          left: -45px;
+          width: 250px;
+          height: 200px;
+          padding: -50px;
+      }
+  `;
+  
+  @query('#canvas')
+  private _canvas!: HTMLCanvasElement;
+  
+  override firstUpdated() {
+    const r = new window.rive.Rive({
+      src: "./publish_button.riv", // Path to your .riv file
+      canvas: this._canvas,
+      autoplay: true,
+      stateMachines: "State Machine 1",
+      onLoad: () => {
+        r.resizeDrawingSurfaceToCanvas();
+      },
+    });
+  }
+
+  override render() {
+    return html`
+      <div class="button-container">
+        <canvas id="canvas" width="50" height="50"></canvas>
+      </div>
+    `;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'play-button': PlayButton;
+  }
+}
+
+interface Rive {
+  new (options: {
+    src: string;
+    canvas: HTMLCanvasElement;
+    autoplay: boolean;
+    stateMachines: string;
+    onLoad: () => void;
+    artboard?: string;
+  }): {
+    resizeDrawingSurfaceToCanvas: () => void;
+  };
+}
+
+declare global {
+  interface Window {
+    rive: {
+      Rive: Rive;
+    };
+  }
+}
